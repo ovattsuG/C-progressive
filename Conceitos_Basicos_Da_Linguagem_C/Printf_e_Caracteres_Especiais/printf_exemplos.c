97c370658af7f339e619ecdf51151c23b25690a8
Gustavo Correa <gustaalves32@gmail.com>
2025-08-14 19:00:12 -0300

    Hello world e função printf

---
+++ Conceitos_Basicos_Da_Linguagem_C/Printf_e_Caracteres_Especiais/printf_exemplos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void){
+    printf("Aspas Duplas: \" \n");
+    printf("Barra: \\ \n");
+    // printf("Carriage return: \r \n");
+    // getchar();
+    // printf("Antes do \\t \t Depois do \\t \n");
+    // printf("\7 \a");
+    return 0;
+}
---
+++ Conceitos_Basicos_Da_Linguagem_C/hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello world \n");
+    // criando um comentario em nosso codigo ;D
+    return 0;
+}
