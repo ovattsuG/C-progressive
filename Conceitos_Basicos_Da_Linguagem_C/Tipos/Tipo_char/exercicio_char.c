547485c727abe5fd5547ccd86cc09bde1416b797
Gustavo Correa <gustaalves32@gmail.com>
2025-08-16 15:07:20 -0300

    Aula e exercicios sobre Tipos em C

---
+++ Conceitos_Basicos_Da_Linguagem_C/Tipos/Tipo_char/exercicio_char.c
@@ -0,0 +1,11 @@
+/*
+Exercício:
+Escreva seu nome usando apenas a função printf, %c e números. Use a tabela ASCII.
+*/
+#include <stdio.h>
+
+int main(){
+    
+    printf("%c%c%c%c%c%c%c", 71, 85, 83, 84,65, 86, 79);
+    
+}
---
+++ Conceitos_Basicos_Da_Linguagem_C/Tipos/Tipo_float_e_double/float_e_double.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    float pi = 3.14;
+    double piDouble = 3.1415926535897932384626433832795;
+    printf("Valor de pi %.2f\n", pi);
+    printf("Valor de pi mais preciso %.7f\n", piDouble);
+}
