93124e8cfcb981029a84fb068510db8dcc88cd90
Gustavo Correa <gustaalves32@gmail.com>
2025-09-03 09:24:26 -0300

    operadores de incremento e decramento

---
+++ Teste_Condicional_e_Controle_de_Fluxo/Operadores/operadores_de_incremento_e_decramento.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{       int a=1,
+            b=1;
+
+        printf("Valor inicial de a = %d\n", a);
+        printf("Valor inicial de b = %d\n", b);
+
+        printf("\nIncrementando: a++\n");
+        a++;
+
+        printf("Decrementando: b--\n");
+        b--;
+
+        printf("\nAgora a = %d\n", a);
+        printf("Agora b = %d\n",b);
+
+}
