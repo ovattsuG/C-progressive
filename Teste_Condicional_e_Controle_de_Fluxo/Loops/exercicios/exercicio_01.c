55708c965ef3c91027e52d7f5c2ff01f1092ae80
Gustavo Correa <gustaalves32@gmail.com>
2025-09-04 14:18:27 -0300

    Econtrar os numeros impares exercicio_01

---
+++ Teste_Condicional_e_Controle_de_Fluxo/Loops/exercicios/exercicio_01.c
@@ -0,0 +1,23 @@
+/*
+1. Programa em C que mostra os números ímpares
+Escreva um aplicativo em C mostra todos os números ímpares de 1 até 100.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int numero = 1;
+    printf("Os numeros Impares entre 0 e 100 são: \n");
+    while (numero <= 100)
+    {
+        if((numero % 2) == 1)
+        {
+            printf("%d \n", numero);
+        }
+
+        numero = numero + 1;
+    
+    }
+    
+}
