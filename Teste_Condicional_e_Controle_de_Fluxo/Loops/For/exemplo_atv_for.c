e84b75b65f5c4b0957a9bae877eaacd39a056f1a
Gustavo Correa <gustaalves32@gmail.com>
2025-09-08 21:34:28 -0300

    loop for

---
+++ Teste_Condicional_e_Controle_de_Fluxo/Loops/For/exemplo_atv_for.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int termos,
+            razao,
+            inicial,
+            count;
+
+        printf("Número de termos da P.A: ");
+        scanf("%d", &termos);
+
+        printf("Razão da P.A: ");
+        scanf("%d", &razao);
+
+        printf("Elemento inicial da P.A: ");
+        scanf("%d", &inicial);
+
+        for(count = 1; count <= termos ; count++)
+            printf("Termo %d: %d\n", count, (inicial + (count-1)*razao) );
+
+}
